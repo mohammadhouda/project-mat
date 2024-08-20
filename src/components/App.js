8aee921c78557f1a8282773b9eb5ebc2ee94cc6a
mhamad-chahine <mohamad.abouchahine@outlook.com>
2024-08-20 14:23:20 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,35 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import AdminPage from './AdminPage';
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = (user, password) => {
+    // Authentication logic here the user and password for now 'admin'
+
+    if (user === 'admin' && password === 'admin') {
+      setIsAuthenticated(true);
+    } else {
+      alert('Invalid credentials');
+    }
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={!isAuthenticated ? <LoginForm onLogin={handleLogin} /> : <Navigate to="/admin" />}
+        />
+        <Route
+          path="/admin"
+          element={isAuthenticated ? <AdminPage /> : <Navigate to="/" />}
+        />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
