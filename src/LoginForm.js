252a093e507c5630b24dbd5877f1f0222da2b22b
mhamad-chahine <mohamad.abouchahine@outlook.com>
2024-08-20 14:04:04 +0300

    Create LoginForm.js

---
+++ src/LoginForm.js
@@ -0,0 +1,53 @@
+
+import React, { useState } from 'react';
+import './LoginForm.css';
+import logo from './logo.png';
+
+const LoginForm = ({ onLogin }) => {
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (onLogin) {
+      onLogin(user, password);
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <div className='title-container'>
+          <h1>Login Page</h1>
+        </div>
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+        <div className="input-group">
+          <label htmlFor="user">User:</label>
+          <input
+            type="text"
+            id="user"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="button-container">
+          <a href="/forgot-password" className="forgot-password">Forgot Password?</a>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
